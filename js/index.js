cc927e2b9f771caed91893445dda8f0456085402
Nadeesha Ruwandima <114317388+Nadeesha699@users.noreply.github.com>
2024-12-20 02:10:40 +0530

    saved

--- js/index.js
+++ js/index.js
@@ -1,24 +1,12 @@
 function clickClear() {
-    document.getElementById("show").innerText = "";
-  }
+  document.getElementById("show").innerText = "";
+}
 
-  function clickCal(e) {
-    if (e === 11) {
-      document.getElementById("show").innerText += "*";
-    } else if (e === 12) {
-      document.getElementById("show").innerText += "+";
-    } else if (e === 13) {
-      document.getElementById("show").innerText += "-";
-    } else if (e === 14) {
-      document.getElementById("show").innerText += ".";
-    } else if (e === 15) {
-      let s = document.getElementById("show").innerText;
-      document.getElementById("show").innerText = eval(s);
-    } else if (e === 16) {
-      document.getElementById("show").innerText += "%";
-    } else if (e === 17) {
-      document.getElementById("show").innerText += "/";
-    } else {
-      document.getElementById("show").innerText += e;
-    }
+function clickCal(e) {
+  if (e === "=") {
+    let s = document.getElementById("show").innerText;
+    document.getElementById("show").innerText = eval(s);
+  } else {
+    document.getElementById("show").innerText += e;
   }
+}
